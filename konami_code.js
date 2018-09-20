67ba55b0b999bcbc1c179f501b24a1c140403eae
Emily Friedlander <emily@emilyfriedlander.com>
2018-09-20 01:04:29 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,31 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+// function init() {
+// function keySequince (e){
+//   userCode.push(e.which)
+//   alert('the key press is ' +e.which)
+// }
 
+// Write your JavaScript code inside the init() function
+
+// document.addEventListener('keydown', function () {
+// alert('key pressed!')
+// })
+// }
+
+const alphabet= [65, 66, 67];
+let index=0;
+
+function onKeyDownhandler(e) {
+  const key = parseInt(e.which);
+  if (key === code[index]){
+    index++;
+  }
+  else {
+    index=0;
+  }
 }
+
+document.addEventListener('keydown', onKeyDownhandler)
+//   }
+// }
